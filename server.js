5efc40ec1a15090ffa30c211df3ede46c19c3448
bbr-fs369 <baburao.bolla@fullstack369.com>
2022-12-03 16:53:54 +0530

    default route msg updated

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-const UserRouter = require("./routes/user-route");
+const UserRouter = require('./routes/user-route');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(express.json());
 
-app.use("/health", (req, res) => {
-  res.status(200).send("System is Up");
+app.use('/', (req, res) => {
+  res.status(200).send('Nodejs Server is Running');
 });
 
-app.use("/user/", UserRouter);
+app.use('/user/', UserRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started on Port: ${PORT}`);
